52960f1d500f8d8bd0732c92956b036b4cab40e9
GeorgKreuzmayr <g.kreuzmayr@gmail.com>
2020-10-05 09:24:36 +0200

    revert later
    Signed-off-by: GeorgKreuzmayr <g.kreuzmayr@gmail.com>

--- Travis/test_windows.ps1
+++ Travis/test_windows.ps1
@@ -18,4 +18,7 @@ else
     Write-Output "Git Tag has to resemble a package version (e.g. 1.0.0)."
     exit 1
 }
-dotnet test -c Release
+#dotnet test -c Release
+dotnet nuget push ($PWD.Path + "\ArchUnitNET\nupkgs\TngTech.ArchUnitNET.*.nupkg") -k "test123" -s $source
+dotnet nuget push ($PWD.Path + "\ArchUnitNET.xUnit\nupkgs\TngTech.ArchUnitNET.xUnit.*.nupkg") -k "test123" -s $source
+dotnet nuget push ($PWD.Path + "\ArchUnitNET.NUnit\nupkgs\TngTech.ArchUnitNET.xUnit.*.nupkg") -k "test123" -s $source
