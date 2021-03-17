8c7966740603c3dc223cf5d43d735f2bd392637f
Georg Kreuzmayr <georg.kreuzmayr@tngtech.com>
2021-03-17 15:00:10 +0100

    Add automatic update of gh-pages

--- Travis/setup-ssh.sh
+++ Travis/setup-ssh.sh
@@ -5,7 +5,7 @@ set -ev
 declare -r SSH_FILE="$(mktemp -u $HOME/.ssh/travis_temp_ssh_key_XXXX)"
 # Decrypt the file containing the private key (put the real name of the variables)
 
-openssl aes-256-cbc -k "83b31c2adcc95d69722405111a0e1b59ec8f62d301feb94cc0a62247925799c3" \
+openssl aes-256-cbc -k "$travis_key_password" \
  -d -md sha256 -a -in "Travis/travis_key.enc" -out "$SSH_FILE"
 
 # Enable SSH authentication
