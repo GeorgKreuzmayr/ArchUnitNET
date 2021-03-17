07a0c2f9546c9da7710daea306a14dcca176c31b
Georg Kreuzmayr <g.kreuzmayr@gmail.com>
2021-03-17 19:57:54 +0100

    Fix path in deploy_documentation.sh
    Signed-off-by: Georg Kreuzmayr <g.kreuzmayr@gmail.com>

--- Travis/deploy_documentation.sh
+++ Travis/deploy_documentation.sh
@@ -3,7 +3,7 @@
 # Generate the docs only if master, the travis_build_docs is true and we can use secure variables
 
 if [[ "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = "false" ]] ; then
-  chmod +x ./Travis/setup-ssh.sh ./Travis/generate-documentation.sh
+  chmod +x "$TRAVIS_BUILD_DIR/Travis/setup-ssh.sh" "$TRAVIS_BUILD_DIR./Travis/generate-documentation.sh"
   source ./Travis/setup-ssh.sh || travis_terminate 1
   ./Travis/generate-documentation.sh || travis_terminate 1
 fi
