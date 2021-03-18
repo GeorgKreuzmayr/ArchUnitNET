0976c8449cc71fb3b537d5ade5be057c19b591a0
Georg Kreuzmayr <g.kreuzmayr@gmail.com>
2021-03-18 11:48:47 +0100

    Use $TRAVIS_TAG instead of deploy script
    Signed-off-by: Georg Kreuzmayr <g.kreuzmayr@gmail.com>

--- Travis/deploy_documentation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Generate the docs only if master, the travis_build_docs is true and we can use secure variables
-
-chmod +x "$TRAVIS_BUILD_DIR/Travis/setup-ssh.sh" "$TRAVIS_BUILD_DIR./Travis/generate-documentation.sh"
-source ./Travis/setup-ssh.sh || travis_terminate 1
-./Travis/generate-documentation.sh || travis_terminate 1
