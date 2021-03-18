5a223a4fe75766414c80b19d9d13106043245d03
Georg Kreuzmayr <g.kreuzmayr@gmail.com>
2021-03-18 12:46:20 +0100

    Add enter to git clone
    Signed-off-by: Georg Kreuzmayr <g.kreuzmayr@gmail.com>

--- Travis/generate-documentation.sh
+++ Travis/generate-documentation.sh
@@ -47,7 +47,7 @@ DOXYFILE="$TRAVIS_BUILD_DIR/documentation/Doxyfile"
 
 
 # Get the current gh-pages branch
-git clone -b gh-pages git@github.com:$GH_REPO_ORG/$GH_REPO_NAME.git code_docs
+echo | git clone -b gh-pages git@github.com:$GH_REPO_ORG/$GH_REPO_NAME.git code_docs
 cd code_docs
 
 
