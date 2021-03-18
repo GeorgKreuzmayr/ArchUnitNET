1a2244272ec981b27212c690bed9fe34d1fe8bf9
Georg Kreuzmayr <g.kreuzmayr@gmail.com>
2021-03-18 16:07:04 +0100

    Fix malformed ssh key
    Signed-off-by: Georg Kreuzmayr <g.kreuzmayr@gmail.com>

--- Travis/setup-ssh.sh
+++ Travis/setup-ssh.sh
@@ -4,7 +4,7 @@
 set -ev
 declare -r SSH_FILE="$(mktemp -u $HOME/.ssh/travis_temp_ssh_key_XXXX)"
 
-echo "$SSH_PRIVATE" > "$SSH_FILE"
+echo -e "$SSH_PRIVATE" > "$SSH_FILE"
 
 # Enable SSH authentication
 chmod 600 "$SSH_FILE" \
@@ -18,5 +18,5 @@ ssh-keyscan github.com >> "$HOME/.ssh/known_hosts"
 eval $(ssh-agent -s)
 
 chmod +x "$TRAVIS_BUILD_DIR/Travis/ssh-add-helper.sh"
-echo "$SSH_PASSPHRASE"| SSH_ASKPASS="$TRAVIS_BUILD_DIR/Travis/ssh-add-helper.sh" ssh-add "$SSH_FILE" 2> /dev/null
+ssh-add "$SSH_FILE"
 
