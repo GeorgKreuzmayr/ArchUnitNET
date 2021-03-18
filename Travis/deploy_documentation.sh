f4691ca3282e6431a64a94994bebf00b9b23752c
Georg Kreuzmayr <g.kreuzmayr@gmail.com>
2021-03-18 12:24:34 +0100

    Update deploy_doc script
    Signed-off-by: Georg Kreuzmayr <g.kreuzmayr@gmail.com>

--- Travis/deploy_documentation.sh
+++ Travis/deploy_documentation.sh
@@ -4,4 +4,4 @@
 
 chmod +x "$TRAVIS_BUILD_DIR/Travis/setup-ssh.sh" "$TRAVIS_BUILD_DIR/Travis/generate-documentation.sh"
 source "$TRAVIS_BUILD_DIR/Travis/setup-ssh.sh" || travis_terminate 1
-"$TRAVIS_BUILD_DIR./Travis/generate-documentation.sh" || travis_terminate 1
+"$TRAVIS_BUILD_DIR/Travis/generate-documentation.sh" || travis_terminate 1
