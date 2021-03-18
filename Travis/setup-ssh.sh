78d36e5325a8e405aae1a03c98ba48bacc49eded
Georg Kreuzmayr <g.kreuzmayr@gmail.com>
2021-03-18 12:38:39 +0100

    skip cleanup
    Signed-off-by: Georg Kreuzmayr <g.kreuzmayr@gmail.com>

--- Travis/setup-ssh.sh
+++ Travis/setup-ssh.sh
@@ -3,9 +3,8 @@
 
 set -ev
 declare -r SSH_FILE="$(mktemp -u $HOME/.ssh/travis_temp_ssh_key_XXXX)"
-# Decrypt the file containing the private key (put the real name of the variables)
 
-echo "$SSH_PRIVATE" > $SSH_FILE
+echo "$SSH_PRIVATE" > "$SSH_FILE"
 
 # Enable SSH authentication
 chmod 600 "$SSH_FILE" \
